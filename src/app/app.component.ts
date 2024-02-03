97f936e1785a2b12df03d735f5595e6c475064e6
hariharn12 <140273143+hariharn12@users.noreply.github.com>
2024-02-03 20:11:16 +0530

    create a add and remove button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'forms';
+
+  forms = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required],),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    dob: new FormControl(''),
+    address:new FormArray([
+      new FormControl('Addr1'),
+     
+    ])
+  });
+  getAddress(){
+    return this.forms.get('address') as FormArray;
+  }
+  
+  addAddress(){
+    this.getAddress().push(new FormControl("Addr"))
+  }
+  removeAddress(index: number) {
+    this.getAddress().removeAt(index);
+  }
+
+  handleSubmit() {
+    console.log(this.forms.value);
+    console.log(this.forms.valid);
+    if(this.forms.invalid){
+      window.alert('fill this forms')
+    }
+    
+  }
 }
